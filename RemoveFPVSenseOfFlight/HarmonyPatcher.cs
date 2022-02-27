6ceb74990d907794ef20c4637fecaea4fc68f959
Daniel Sloggett <48067784+dslogget@users.noreply.github.com>
2022-02-27 01:48:08 +0000

    Initial Commit

---
+++ RemoveFPVSenseOfFlight/HarmonyPatcher.cs
@@ -0,0 +1,53 @@
+﻿using BalsaCore;
+using HarmonyLib;
+using IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+
+namespace CloverTech
+{
+    class HarmonyContainer
+    {
+        public static Harmony harmony = new Harmony("Balsa.CloverTech.RemoveFPVSenseOfFlight");
+
+        public static void DoPatches()
+        {
+#if DEBUG
+            Harmony.DEBUG = true;
+#endif
+            harmony.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(FloatingCameraFX.Modules.FPVSenseOfFlight), "UpdateLocalPosition")]
+        class RemoveFPVSenseOfFlightPosPatch
+        {
+            static bool Prefix(Vector3 defaultPos,
+                               Vector3 currPos,
+                               float m,
+                               Vector3 wpos,
+                               ref Vector3 __result)
+            {
+                __result = currPos;
+                return false;
+            }
+        }
+        [HarmonyPatch(typeof(FloatingCameraFX.Modules.FPVSenseOfFlight), "UpdateLocalRotation")]
+        class RemoveFPVSenseOfFlightRotPatch
+        {
+            static bool Prefix(Quaternion defaultRot,
+                               Quaternion currRot,
+                               float m,
+                               Quaternion wRot,
+                               ref Quaternion __result)
+            {
+                __result = currRot;
+                return false;
+            }
+        }
+
+    }
+}
---
+++ RemoveFPVSenseOfFlight/Loader.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using BalsaCore;
+using IO;
+using UI;
+using UI.MMX.Data;
+using System.Collections.Generic;
+using System.Reflection;
+using System;
+using System.IO;
+
+namespace CloverTech
+{
+    public class RemoveFPVSenseOfFlight : MonoBehaviour
+    {
+        public void Start()
+        {
+            DontDestroyOnLoad(this);
+            LogW("Test");
+#if DEBUG
+            Debug.LogError("Init");
+#endif
+            HarmonyContainer.DoPatches();
+        }
+
+        public void LogI(string message)
+        {
+            Debug.Log($"[RemoveFPVSenseOfFlight] {message}");
+        }
+        public void LogW(string message)
+        {
+            Debug.LogWarning($"[RemoveFPVSenseOfFlight] {message}");
+        }
+        public void LogE(string message)
+        {
+            Debug.LogError($"[RemoveFPVSenseOfFlight] {message}");
+        }
+    }
+    
+
+    [BalsaAddon]
+    public class Loader
+    {
+        private static GameObject go = null;
+        private static bool initialised = false;
+
+        [BalsaAddonInit]
+        public static void BalsaInit()
+        {
+            if (!initialised)
+            {
+                Debug.Log("[RemoveFPVSenseOfFlight] Creating GameObject");
+                go = new GameObject("CloverTech::RemoveFPVSenseOfFlight");
+                go.AddComponent<RemoveFPVSenseOfFlight>();
+                initialised = true;
+            }
+        }
+
+        [BalsaAddonInit(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaInitFlight()
+        {
+
+        }
+
+        [BalsaAddonFinalize(invokeTime = AddonInvokeTime.Flight)]
+        public static void BalsaFinalizeFlight()
+        {
+
+        }
+        //Game exit
+        [BalsaAddonFinalize]
+        public static void BalsaFinalize()
+        {
+            go.DestroyGameObject();
+        }
+
+    }
+}
